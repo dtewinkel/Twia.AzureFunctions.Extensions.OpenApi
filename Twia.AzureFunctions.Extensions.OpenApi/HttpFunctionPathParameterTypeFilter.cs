7d8ac90767d92c9cd3753f25927ebdf9c6ec61f8
Daniël te Winkel <git@twia.nl>
2020-04-27 20:58:25 +0200

    Added filter to fix type.

---
+++ Twia.AzureFunctions.Extensions.OpenApi/HttpFunctionPathParameterTypeFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Twia.AzureFunctions.Extensions.OpenApi
+{
+    public class HttpFunctionPathParameterTypeFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            foreach (var operationParameter in operation.Parameters)
+            {
+                if (operationParameter.In == ParameterLocation.Path)
+                {
+                    var description = context.ApiDescription.ParameterDescriptions.FirstOrDefault(parameter => parameter.Name == operationParameter.Name);
+                    operationParameter.Schema = context.SchemaGenerator.GenerateSchema(description?.Type, context.SchemaRepository);
+                }
+            }
+        }
+    }
+
+}
